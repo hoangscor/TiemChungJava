03e3d3cd72585aae3cc3139bfd944b5c2f21313d
Chau Ngoc <chaungoc7909@gmail.com>
2025-03-11 13:00:08 +0700

    them lai

---
+++ src/main/java/edu/uth/tiemchungjava/TiemChungJavaApplication.java
@@ -0,0 +1,13 @@
+package edu.uth.tiemchungjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiemChungJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiemChungJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/uth/tiemchungjava/TiemChungJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uth.tiemchungjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiemChungJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
