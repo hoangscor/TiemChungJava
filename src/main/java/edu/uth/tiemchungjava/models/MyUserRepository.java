53830aa4ea3b565224fd56d80c05f818fbbee2df
Lê Huy Hoàng <156869304+hoangscor@users.noreply.github.com>
2025-03-30 18:59:49 +0700

    interface Repository User

---
+++ src/main/java/edu/uth/tiemchungjava/models/MyUserRepository.java
@@ -0,0 +1,10 @@
+package edu.uth.tiemchungjava.models;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MyUserRepository extends JpaRepository<MyUser, Long> {
+
+    Optional<MyUser> findByUsername(String username);
+}
