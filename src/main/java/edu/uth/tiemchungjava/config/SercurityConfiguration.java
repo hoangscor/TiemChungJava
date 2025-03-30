8dd772179d48129ebf9e0b76cc10a8940ad83568
Lê Huy Hoàng <156869304+hoangscor@users.noreply.github.com>
2025-03-30 23:34:47 +0700

    add again SercurityConfiguration.java

---
+++ src/main/java/edu/uth/tiemchungjava/config/SercurityConfiguration.java
@@ -0,0 +1,72 @@
+package edu.uth.tiemchungjava.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SercurityConfiguration {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(registry->{
+            registry.requestMatchers("/indexx", "/register/**", "/img/**", "/assets/**", "/bootstrap/**", "/css/**", "/js/**", "/pages/**", "/webfonts/**").permitAll();
+            registry.requestMatchers("/admin/**, ").hasRole("ADMIN");
+            registry.requestMatchers("/user/**").hasRole("USER");
+            registry.anyRequest().authenticated();
+        })
+                .formLogin(formLogin -> formLogin.permitAll())
+                .build();
+    }
+
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        UserDetails normalUser = User.builder()
+//                .username("gc")
+//                .password("$2a$12$9hSP9pPC4kYrwuU9F3bL8O.EW40i9GgdUFwiWsYlbdMuQcn3fbUNC") //1234
+//                .roles("USER")
+//                .build();
+//        UserDetails adminUser = User.builder()
+//                .username("admin")
+//                .password("$2a$12$Ud52w/IBXQSkQ0KLNUZ7sefxkwsx6h4.hPDhp87/fPiW0fP4FdjVu") //9876
+//                .roles("ADMIN", "USER")
+//                .build();
+//        return new InMemoryUserDetailsManager(normalUser, adminUser);
+//    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return userDetailsService;
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
