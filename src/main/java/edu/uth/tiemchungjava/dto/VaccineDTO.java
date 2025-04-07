2adee4ee520befc10239bdd1c4f5b2c9e2e72272
Chau Ngoc <chaungoc7909@gmail.com>
2025-04-07 19:32:26 +0700

    fix dto

--- src/main/java/edu/uth/tiemchungjava/dto/VaccineDTO.java
+++ src/main/java/edu/uth/tiemchungjava/dto/VaccineDTO.java
@@ -1,17 +1,33 @@
 package edu.uth.tiemchungjava.dto;
+
+import jakarta.persistence.*;
+
 public class VaccineDTO {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String name;
     private String description;
     private String ageGroup;
     private Integer price;
     private String img_url;
-
-    public VaccineDTO(String name, String description, String ageGroup, Integer price, String imgUrl) {
+    public VaccineDTO(Long id, String name, String description, Integer price, String img_url) {
+        this.id = id;
         this.name = name;
         this.description = description;
         this.ageGroup = ageGroup;
         this.price = price;
-        this.img_url= imgUrl;
+        this.img_url= img_url;
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getName() {
