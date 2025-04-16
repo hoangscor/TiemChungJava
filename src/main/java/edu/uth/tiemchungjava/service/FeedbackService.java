e3f1bb677ecd689fe7d152e9c1136fe7587663fa
Chau Ngoc <chaungoc7909@gmail.com>
2025-04-16 22:43:56 +0700

    tao feedback4

---
+++ src/main/java/edu/uth/tiemchungjava/service/FeedbackService.java
@@ -0,0 +1,23 @@
+package edu.uth.tiemchungjava.service;
+
+import edu.uth.tiemchungjava.models.Feedback;
+import edu.uth.tiemchungjava.repository.FeedbackRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FeedbackService {
+
+    @Autowired
+    private FeedbackRepository feedbackRepository;
+
+    public Feedback saveFeedback(Feedback feedback) {
+        return feedbackRepository.save(feedback);
+    }
+
+    public List<Feedback> getAllFeedbacks() {
+        return feedbackRepository.findAll();
+    }
+}
