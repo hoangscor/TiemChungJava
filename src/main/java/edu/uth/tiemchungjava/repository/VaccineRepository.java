64b32c0b8369d7a52bdd6288801012c1335cdd61
Chau Ngoc <chaungoc7909@gmail.com>
2025-04-21 00:15:43 +0700

    sua 2

--- src/main/java/edu/uth/tiemchungjava/repository/VaccineRepository.java
+++ src/main/java/edu/uth/tiemchungjava/repository/VaccineRepository.java
@@ -2,6 +2,7 @@ package edu.uth.tiemchungjava.repository;
 
 import edu.uth.tiemchungjava.models.Vaccine;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 public interface VaccineRepository extends JpaRepository<Vaccine, Long> {
@@ -11,4 +12,6 @@ public interface VaccineRepository extends JpaRepository<Vaccine, Long> {
 
     // Lấy tất cả và sắp xếp theo nhóm tuổi -> tên vắc xin
     List<Vaccine> findAllByOrderByAgeGroupAscNameAsc();
+    @Query("SELECT DISTINCT v.ageGroup FROM Vaccine v")
+    List<String> findDistinctAgeGroups();
 }
